150a3c319668762d617b43f2b0c0eb94d535016b
EC2 Default User <ec2-user@ip-172-31-4-8.ec2.internal>
2020-11-06 19:34:36 +0000

    week9

---
+++ week9/wa09a.js
@@ -0,0 +1,25 @@
+const { Client } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config({path: '../.env'});  
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'jtroxel';
+db_credentials.host = 'data-structures.cj2bazt69614.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'aa';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+console.log(process.env.AWSRDS_PW)
+// Connect to the AWS RDS Postgres database
+const client = new Client(db_credentials);
+client.connect();
+
+// Sample SQL statement to create a table: 
+var thisQuery = "CREATE TABLE sensorData ( sensorValue double precision, sensorTime timestamp DEFAULT current_timestamp );";
+// Sample SQL statement to delete a table: 
+// var thisQuery = "DROP TABLE aalocations;"; 
+
+client.query(thisQuery, (err, res) => {
+    console.log(err, res);
+    client.end();
+});
---
+++ week9/wa09b.js
@@ -0,0 +1,47 @@
+var request = require('request');
+const { Client } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config({path: '../.env'});
+
+// PARTICLE PHOTON
+var device_id = process.env.PHOTON_ID;
+var access_token = process.env.PHOTON_TOKEN;
+var particle_variable = 'temp';
+var device_url = 'https://api.particle.io/v1/devices/' + device_id + '/' + particle_variable + '?access_token=' + access_token;
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'jtroxel';
+db_credentials.host = 'data-structures.cj2bazt69614.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'aa';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+console.log(process.env.AWSRDS_PW)
+
+var getAndWriteData = function() {
+    
+    // Make request to the Particle API to get sensor values
+    request(device_url, function(error, response, body) {
+        
+        // Store sensor value(s) in a variable
+        var sv = JSON.parse(body).result;
+        
+        // Connect to the AWS RDS Postgres database
+        const client = new Client(db_credentials);
+        client.connect();
+
+        // Construct a SQL statement to insert sensor values into a table
+        // var thisQuery = "INSERT INTO sensorData VALUES (" + sv + ", DEFAULT);";
+        var thisQuery = "SELECT * FROM sensorData;";
+        console.log(thisQuery); // for debugging
+
+        // Connect to the AWS RDS Postgres database and insert a new row of sensor values
+        client.query(thisQuery, (err, res) => {
+            console.log(err, res);
+            client.end();
+        });
+    });
+};
+
+// write a new row of sensor data every five minutes
+setInterval(getAndWriteData, 300000);
